4a1247ec506300a7d0cd74d02532b97b68f370fc
kocheshev <kocheshevk@gmail.com>
2021-12-21 16:38:10 +0300

    Update script.js

--- NY2022/script.js
+++ NY2022/script.js
@@ -1,4 +1,4 @@
-import THREE from "https://cdn.skypack.dev/pin/three@v0.135.0-pjGUcRG9Xt70OdXl97VF/mode=imports,min/optimized/three.js";
+import THREE from "https://cdn.skypack.dev/pin/three@v0.135.0";
 import {
   BloomEffect,
   EffectComposer,
